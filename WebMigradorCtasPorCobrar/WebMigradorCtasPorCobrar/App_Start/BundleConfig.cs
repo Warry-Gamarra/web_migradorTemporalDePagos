af982128269e983ad188ed4422c0c54667aadf5f
wgamarra <wgamarra2310@gmail.com>
2022-03-04 17:19:22 -0500

    layout bootstrap 4

--- WebMigradorCtasPorCobrar/WebMigradorCtasPorCobrar/App_Start/BundleConfig.cs
+++ WebMigradorCtasPorCobrar/WebMigradorCtasPorCobrar/App_Start/BundleConfig.cs
@@ -8,23 +8,52 @@ namespace WebMigradorCtasPorCobrar
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/content/css").Include(
+                "~/Assets/bootstrap/css/bootstrap.min.css",
+                "~/Assets/grid-mvc/css/Gridmvc.css",
+                "~/Assets/toastr/css/toastr.min.css",
+                "~/Assets/application/css/main.css",
+                "~/Assets/application/css/loaders.css",
+                "~/Assets/application/css/sidebar.css"));
+
+            bundles.Add(new StyleBundle("~/content/datetime").Include(
+                    "~/Assets/bootstrap-datepicker/css/bootstrap-datepicker.css",
+                    "~/Assets/bootstrap-datepicker/css/bootstrap-timepicker.css"));
+
+            bundles.Add(new StyleBundle("~/content/fonts").Include(
+                "~/Assets/font-awesome/css/font-awesome.min.css",
+                "~/Assets/bootstrap/css/bootstrap-icons.css"));
+
+            bundles.Add(new StyleBundle("~/content/select").Include(
+                "~/Assets/select2/css/select2.min.css",
+                "~/Assets/select2/css/select2-bootstrap4.min.css"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Assets/jquery/jquery-3.3.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Assets/jquery/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                "~/Assets/bootstrap/js/popper.min.js",
+                "~/Assets/bootstrap/js/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/select").Include(
+                "~/Assets/select2/js/select2.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datetime").Include(
+                "~/Assets/bootstrap-datepicker/js/bootstrap-datepicker.js",
+                "~/Assets/bootstrap-datepicker/js/bootstrap-datepicker.es.js",
+                "~/Assets/bootstrap-datepicker/js/bootstrap-timepicker.js",
+                "~/Assets/bootstrap-datepicker/js/datetimepicker.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Assets/grid-mvc/js/gridmvc.js",
+                "~/Assets/toastr/js/toastr.min.js",
+                "~/Assets/toastr/js/toastr.config.js",
+                "~/Assets/application/js/main.js",
+                "~/Assets/application/js/sidebar.js"));
         }
     }
 }
