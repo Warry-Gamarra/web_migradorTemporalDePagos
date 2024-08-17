e5ab6db941f8fb1e4854ce5a1e54a4b0e63f9b87
wgamrra linux mint <wgamarra@unfv.linux>
2024-08-17 00:41:10 -0500

    modificacion tasas data

--- database/BD_MigracionTempPagos/Migracion Tasas/BD_OCEF_MigracionTP - 02_Data.sql
+++ database/BD_MigracionTempPagos/Migracion Tasas/BD_OCEF_MigracionTP - 02_Data.sql
@@ -4,8 +4,8 @@ GO
 UPDATE TC_ProcedenciaData SET T_ProcedenciaDesc = 'TASAS' WHERE I_ProcedenciaID = 4
 GO
 
-INSERT INTO TC_CatalogoTabla (I_TablaID, T_TablaNom) VALUES (8, 'TR_Tasas_Cp_Des')
-INSERT INTO TC_CatalogoTabla (I_TablaID, T_TablaNom) VALUES (9, 'TR_Tasas_Cp_Pri')
+--INSERT INTO TC_CatalogoTabla (I_TablaID, T_TablaNom) VALUES (8, 'TR_Tasas_Cp_Des')
+--INSERT INTO TC_CatalogoTabla (I_TablaID, T_TablaNom) VALUES (9, 'TR_Tasas_Cp_Pri')
 INSERT INTO TC_CatalogoTabla (I_TablaID, T_TablaNom) VALUES (10, 'TR_Tasas_Ec_Obl')
 INSERT INTO TC_CatalogoTabla (I_TablaID, T_TablaNom) VALUES (11, 'TR_Tasas_Ec_Det')
 INSERT INTO TC_CatalogoTabla (I_TablaID, T_TablaNom) VALUES (12, 'TR_Tasas_Ec_Det_Pagos')
